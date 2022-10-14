69430b17205258b8bcc95ab80954574194ff04d7
James Ng'ang'a <james@lami.world>
2022-10-14 17:01:47 +0300

    clean up
    removed  unceceereasy files and updated the serveless environment

---
+++ apps/lami-accounts/src/entities/app.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity('apps')
+export class App {
+    @PrimaryColumn()
+    tenant_id: number;
+    
+    @Column()
+    client_id: string;
+    
+    @Column()
+    legacy_client_id: string;
+
+    @Column()
+    user_pool: string;
+
+    @Column()
+    client_secret: string;
+
+    @Column()
+    cognito_userpool_id: string;
+}
