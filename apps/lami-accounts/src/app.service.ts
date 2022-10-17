288dac0647148d75063db0ec4d6ad7cc394b0ab8
James Ng'ang'a <james@lami.world>
2022-10-17 13:06:03 +0300

    create user pool and dependecies
    - create partner/tenant userpool,
    - create resource server and scropes

--- apps/lami-accounts/src/app.service.ts
+++ apps/lami-accounts/src/app.service.ts
@@ -1,8 +1,66 @@
+import { CognitoIdentityProviderClient, CreateResourceServerCommand, CreateUserPoolClientCommand, CreateUserPoolCommand, CreateUserPoolCommandOutput, CreateUserPoolDomainCommand } from '@aws-sdk/client-cognito-identity-provider';
 import { Injectable } from '@nestjs/common';
+import { create } from 'domain';
+import { dataSource } from './data.source';
+import { App } from './entities/app';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private client: CognitoIdentityProviderClient;
+
+  constructor() {
+      this.client = new CognitoIdentityProviderClient({
+          region: process.env.COGNITO_AWS_REGION
+      });
+
+
+  }
+
+  async createTenantUserPoolAndClient(tenantName: string): Promise<any>{
+    // create userpool
+    const createUserPoolCommand = new CreateUserPoolCommand({
+      PoolName: tenantName,
+      UsernameAttributes: ['email'],
+      MfaConfiguration: 'No MFA',
+      
+    });
+    // create user pool
+    const { UserPool }= await this.client.send(createUserPoolCommand);
+
+    // default client
+    const createUserPoolClientCommand = new CreateUserPoolClientCommand({
+      ClientName: 'Default',
+      UserPoolId: UserPool.Id,
+      ExplicitAuthFlows: ['USER_PASSWORD_AUTH'],
+      GenerateSecret: true,
+      AllowedOAuthFlows: ['client_credentials'],
+      SupportedIdentityProviders: ['COGNITO'],
+      // TODO: create scopes
+      AllowedOAuthScopes: []
+    });
+    this.client.send(createUserPoolClientCommand);
+
+    // create userpool domain
+    const createUserpoolDomainCommand = new CreateUserPoolDomainCommand({
+      Domain: tenantName,
+      UserPoolId: UserPool.Id
+    });
+    this.client.send(createUserpoolDomainCommand);
+
+    // create resource server
+    const createResourceServerCommand = new CreateResourceServerCommand({
+      Name:'General API access',
+      UserPoolId: UserPool.Id,
+      Identifier: 'https://localhost:8080/api/v2/',
+      Scopes: [
+        {
+          ScopeName: 'access',
+          ScopeDescription: 'General access to API'
+        }
+      ]
+    });
+
+    return { UserPool };
   }
 }
---
+++ apps/lami-accounts/src/data.source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+import { App } from './entities/app';
+export const dataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.CONFIG_DB_HOST,
+    port: 5432,
+    username: process.env.CONFIG_DB_USER,
+    password: process.env.CONFIG_DB_PASSWORD,
+    database: process.env.CONFIG_DB_DATABASE,
+    entities: [App],
+    synchronize: false
+});
+
+
